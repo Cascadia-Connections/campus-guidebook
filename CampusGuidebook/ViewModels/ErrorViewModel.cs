83998965fa7b0deb3bef44620604a3a118fb8e86
Brian Bansenauer <brianb@cascadia.edu>
2022-04-05 11:58:13 -0700

    initial

---
+++ CampusGuidebook/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CampusGuidebook.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
