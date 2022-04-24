07c3b104cbffc28b9f06a7c62af331215c477020
ABWeb-Dev <abradford.webdev@gmail.com>
2022-04-23 17:57:36 -0700

    Got Jquery to function. Have established an event to return a view back to script when selection is changed from Approved to Denied. Just getting started here.

---
+++ CampusGuidebook/wwwroot/js/EventResponseAjax.js
@@ -0,0 +1,21 @@
+﻿
+
+$(document).ready(function () {
+
+    var isRejected = $("#isRejected").val();
+
+    $("#isRejected").change(function () {
+        isRejected = $("#isRejected").val();
+        alert(isRejected);
+    });
+    //$.ajax({
+    //    type: "POST",
+    //    url: 'https://localhost:7212/',
+    //    dataType: "test",
+    //    success: function (data) {
+
+    //    }
+    //}
+        
+    //)
+});
